8c8d9760c1b10c3a685b542d2608950be4e75847
Kazuyuki Iijima <iijimakazuyuki@gmail.com>
2013-06-03 16:49:09 +0900

    module like a bitset class

---
+++ bitset.f90
@@ -0,0 +1,63 @@
+module bitset_mod
+	implicit none
+	type :: bitset
+		integer :: n
+		integer, allocatable :: set(:)
+	end type
+contains
+	function new_bitset(n) result(bs)
+		type(bitset) :: bs
+		integer, intent(in) :: n
+		bs%n = n
+		allocate(bs%set(1+(n-1)/bit_size(n)))
+	end function
+	
+	subroutine print_bitset(bs)
+		type(bitset) :: bs
+		character(len=bs%n) :: a
+		integer :: i, j
+		a = "";
+		do i=1, bs%n
+			if(btest_bitset(bs,i)) then
+				a = trim(a) // "1"
+			else
+				a = trim(a) // "0"
+			end if
+		end do
+		print *, a
+	end subroutine
+	
+	logical function btest_bitset(bs,idx) result(b)
+		type(bitset), intent(inout) :: bs
+		integer, intent(in) :: idx
+		integer :: i, j
+		i = idx
+		call map_bitset(i,j)
+		b = btest(bs%set(j),i)
+	end function
+	
+	subroutine set_bitset(bs,idx)
+		type(bitset), intent(inout) :: bs
+		integer, intent(in) :: idx
+		integer :: i, j
+		i = idx
+		call map_bitset(i,j)
+		print *, i, j
+		bs%set(j) = ibset(bs%set(j),i)
+	end subroutine
+	
+	subroutine clr_bitset(bs,idx)
+		type(bitset), intent(inout) :: bs
+		integer, intent(in) :: idx
+		integer :: i, j
+		i = idx
+		call map_bitset(i,j)
+		bs%set(j) = ibclr(bs%set(j),i)
+	end subroutine
+	
+	subroutine map_bitset(i,j)
+		integer, intent(inout) :: i, j
+		j = i/bit_size(i) + 1
+		i = i - (j-1)*bit_size(i)
+	end subroutine
+end module

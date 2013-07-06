258c6e15705a2fff3b11aea380de6ba5ad7bb5c3
Kazuyuki Iijima <iijimakazuyuki@gmail.com>
2013-07-06 21:27:32 +0900

    Modify jagged array and add print function

--- array.f90
+++ array.f90
@@ -3,6 +3,7 @@ module array
 	implicit none
 	
 	type :: jag_i
+		integer :: n, m
 		integer, allocatable :: val(:)
 		integer, allocatable :: idx(:)
 	end type
@@ -22,15 +23,17 @@ contains
 	function bitset_to_jag(bs) result(a)
 		type(jag_i) :: a
 		type(bitset), intent(in) :: bs(:)
-		integer :: i, j, k, n, m
-		n = size(bs)
-		allocate(a%idx(0:n))
-		do i=1, n
+		integer :: i, j, k
+		a%n = size(bs)
+		allocate(a%idx(0:a%n))
+		a%idx(0) = 0
+		do i=1, a%n
 			a%idx(i) = a%idx(i-1) + count_bitset(bs(i))
 		end do
-		allocate(a%val(a%idx(n)))
+		a%m = a%idx(a%n)
+		allocate(a%val(a%m))
 		k = 0
-		do i=1, n
+		do i=1, a%n
 			do j=1, bs(i)%n
 				if(btest_bitset(bs(i),j)) then
 					k = k + 1
@@ -39,6 +42,16 @@ contains
 			end do
 		end do
 	end function
+	
+	subroutine print_jag(a)
+		type(jag_i), intent(in) :: a
+		integer :: i, j, si, ei
+		do i=1, a%n
+			si = a%idx(i-1) + 1
+			ei = a%idx(i)
+			if(si <= ei) print *, a%val(si:ei)
+		end do
+	end subroutine
 	
 	integer function count_i(a,x) result(count)
 		integer, intent(in) :: a(:), x
